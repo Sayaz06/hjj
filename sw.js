dfc1e358a25df947ad37100cfad80099bc26bd5b
Sayaz06 <adha86614@gmail.com>
2025-11-19 20:51:58 +0800

    Upgrade cache version and add new icons
    Updated cache version and added new icon references.

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
-const CACHE_NAME = "bulking-cache-v2";
-const urlsToCache = ["./index.html","./manifest.json","./icon.png"];
+const CACHE_NAME = "bulking-cache-v3"; // Naikkan versi lagi, jika v2 gagal
+const urlsToCache = [
+    "./index.html",
+    "./manifest.json",
+    "./iconBulking-192.png", // Tambah rujukan ikon baharu
+    "./iconBulking-512.png"  // Tambah rujukan ikon baharu
+];
 self.addEventListener("install", e=>{ e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(urlsToCache))); });
 self.addEventListener("fetch", e=>{ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))); });
-
